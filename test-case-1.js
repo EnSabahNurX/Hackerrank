3580134af14ffa3e4d25e707ee2139b032a3f888
ricardo <ricklimadealmeida@gmail.com>
2021-03-03 04:52:46 -0300

    new examples from hackerrank

--- test-case-1.js
+++ test-case-1.js
@@ -1,6 +1,104 @@
 
 
 
+
+// function repeatedString(s, n) {
+//     if (s.length == 1 && s[0] == 'a') {
+//         return n
+//     } else if (!s.match(/a/g)) {
+//         return 0
+//     } else {
+//         return Math.floor(n / s.length) * (s.match(/a/g).length) + 
+//         ((n % s.length) ? s.slice(0, n % s.length).split('')
+//         .filter(_ => _ == 'a').length : n % s.length)
+//     }
+//     
+//     
+//     let matched = s.split('').filter((c) => c == 'a').length
+//     let repeats = Math.floor(n/s.length)
+//     let left = n - (repeats * s.length)
+//     return repeats * matched + s.split('').filter((x,idx) => x == 'a' && idx < left).length
+//     
+//     
+//     let sOrig = s;
+//     let fullCountInS = (s.length - s.replace(/a/g, '').length);
+//     let repeat = Math.floor(n / s.length);
+//     let leftOver = n % s.length;
+//     
+//     let count = repeat * fullCountInS;
+//     
+//     s = sOrig.substring(0, leftOver);
+//     let leftOverCount = (s.length - s.replace(/a/g, '').length);
+//     
+//     return count + leftOverCount
+//     
+//     
+// }
+// console.log(repeatedString('a', 1000000000000)) // 1000000000000
+// console.log(repeatedString('aba', 10)) // 7
+// console.log(repeatedString('abcac', 10)) // 4
+// console.log(repeatedString('x', 10000000000)) // 0
+// console.log(repeatedString('kmretasscityylpdhuwjirnqimlkcgxubxmsxpypgzxtenweirknjtasxtvxemtwxuarabssvqdnktqadhyktagjxoanknhgilnm', 736778906400)) // 51574523448
+// console.log(repeatedString('gfcaaaecbg', 547602)) // 164280
+// console.log(repeatedString('jdiacikk', 899491)) // 112436
+// console.log(repeatedString('epsxyyflvrrrxzvnoenvpegvuonodjoxfwdmcvwctmekpsnamchznsoxaklzjgrqruyzavshfbmuhdwwmpbkwcuomqhiyvuztwvq', 549382313570)) // 16481469408
+
+
+// function jumpingOnClouds(c) {
+//     let jump = 0
+//     let i = 0
+//     while (i < c.length - 1) {
+//         if (c[i + 2] == 0)  {
+//             jump++
+//             i += 2
+//         } else {
+//             jump++
+//             i += 1
+//         }
+//     }
+//     return jump
+// }
+// console.log(jumpingOnClouds([0, 0, 1, 0, 0, 1, 0])) // 4
+// console.log(jumpingOnClouds([0, 0, 0, 0, 1, 0])) // 3
+// console.log(jumpingOnClouds([0, 1, 0, 0, 0, 1, 0])) // 3
+
+
+// function viralAdvertising(n) {
+//     let sum = 0
+//     let recipients = 5
+//     for (let i = 1; i <= n; i++) {
+//         sum += Math.floor(recipients / 2)
+//         recipients = Math.floor(recipients / 2) * 3
+//     }
+//     return sum
+//     
+//     return Array(n).fill().map((_,x)=> x + 1)
+//     .reduce(function(result, x) {
+//         result[1] += Math.floor(result[0] / 2)
+//         result[0] =  Math.floor(result[0] / 2) * 3
+//         return result
+//     }, [5, 0])[1]
+// }
+// console.log(viralAdvertising(3)) // 9
+// console.log(viralAdvertising(4)) // 15
+// console.log(viralAdvertising(5)) // 24
+// console.log(viralAdvertising(6)) // 37
+// console.log(viralAdvertising(7)) // 56
+// console.log(viralAdvertising(49)) // 1379192761
+// console.log(viralAdvertising(50)) // 2068789129
+
+
+// function beautifulDays(i, j, k) {
+//     return Array(j - i + 1).fill()
+//     .map((_, idx) => idx + i)
+//     .filter(x => 
+//     (Math.abs(x - Number(x.toString().split('').reverse().join(''))) / k) % 1 == 0)
+//     .length
+// 
+// }
+// console.log(beautifulDays(20, 23, 6))
+
+
 // function domainName(url){
 //     return url.replace(/.*www.|.*\/\/|\..*/g, '')
 //     return url.match(/(?:http(?:s)?:\/\/)?(?:w{3}\.)?([^\.]+)/i)[1]

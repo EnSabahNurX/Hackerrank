71ab3a3e00302dfd0822958544b1669b42d04bab
ricardo <ricklimadealmeida@gmail.com>
2021-04-06 20:48:33 -0300

    implementatio exercises level easy is done

--- test-case-2.js
+++ test-case-2.js
@@ -1,20 +1,27 @@
 function strangeCounter(t) {
-    let cycle = 1
-    let cycleSize = 3
-    let last = 3
-    let current = 1
-    if (t <= last) {
-        for (let i = 1; i <= cycleSize; i++) {
-            if (current == t) {
-                return last
-            }
-            last--
-            current++
-        }
-    }
+    // let snake = 3
+    // while ((2 * snake) - 2 <= t) snake *= 2
+    // return snake - (t - (snake - 2))
 
+    // let snake = 3
+    // while (t > snake) t = t - snake, snake *= 2
+    // return (snake - t + 1)
+
+    let snake = Math.ceil(Math.log2(1 + t / 3))
+    return 3 * (2 ** snake - 1) - t + 1
 }
 console.log(strangeCounter(1))
 console.log(strangeCounter(2))
 console.log(strangeCounter(3))
+console.log(strangeCounter(4))
+console.log(strangeCounter(5))
+console.log(strangeCounter(6))
+console.log(strangeCounter(7))
+console.log(strangeCounter(8))
+console.log(strangeCounter(9))
+console.log(strangeCounter(10))
+console.log(strangeCounter(11))
+console.log(strangeCounter(12))
+console.log(strangeCounter(13))
+console.log(strangeCounter(14))
 

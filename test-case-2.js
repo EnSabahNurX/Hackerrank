ebf5ae6b3a7210928b05379512e30ef8fbba828c
ricardo <ricklimadealmeida@gmail.com>
2021-04-08 16:28:57 -0300

    back to exercises

--- test-case-2.js
+++ test-case-2.js
@@ -1,27 +1,46 @@
-function strangeCounter(t) {
-    // let snake = 3
-    // while ((2 * snake) - 2 <= t) snake *= 2
-    // return snake - (t - (snake - 2))
 
-    // let snake = 3
-    // while (t > snake) t = t - snake, snake *= 2
-    // return (snake - t + 1)
 
-    let snake = Math.ceil(Math.log2(1 + t / 3))
-    return 3 * (2 ** snake - 1) - t + 1
-}
-console.log(strangeCounter(1))
-console.log(strangeCounter(2))
-console.log(strangeCounter(3))
-console.log(strangeCounter(4))
-console.log(strangeCounter(5))
-console.log(strangeCounter(6))
-console.log(strangeCounter(7))
-console.log(strangeCounter(8))
-console.log(strangeCounter(9))
-console.log(strangeCounter(10))
-console.log(strangeCounter(11))
-console.log(strangeCounter(12))
-console.log(strangeCounter(13))
-console.log(strangeCounter(14))
+
+// function notesCount(valor) {
+// let valor = parseInt(gets())
+//     let notes = [[0, 100], [0, 50], [0, 20], [0, 10], [0, 5], [0, 2], [0, 1]]
+//     let rem = valor
+//     console.log(valor)
+//     for (let i in notes) {
+//         if (rem >= notes[i][1]) {
+//             notes[i][0] = Math.floor(rem / notes[i][1])
+//             rem %= notes[i][1]
+//         }
+//         console.log(`${notes[i][0]} nota(s) de R$ ${notes[i][1]},00`)
+//     }
+// }
+// notesCount(576)
+
+
+// function strangeCounter(t) {
+//     // let snake = 3
+//     // while ((2 * snake) - 2 <= t) snake *= 2
+//     // return snake - (t - (snake - 2))
+
+//     // let snake = 3
+//     // while (t > snake) t = t - snake, snake *= 2
+//     // return (snake - t + 1)
+
+//     let snake = Math.ceil(Math.log2(1 + t / 3))
+//     return 3 * (2 ** snake - 1) - t + 1
+// }
+// console.log(strangeCounter(1))
+// console.log(strangeCounter(2))
+// console.log(strangeCounter(3))
+// console.log(strangeCounter(4))
+// console.log(strangeCounter(5))
+// console.log(strangeCounter(6))
+// console.log(strangeCounter(7))
+// console.log(strangeCounter(8))
+// console.log(strangeCounter(9))
+// console.log(strangeCounter(10))
+// console.log(strangeCounter(11))
+// console.log(strangeCounter(12))
+// console.log(strangeCounter(13))
+// console.log(strangeCounter(14))
 
